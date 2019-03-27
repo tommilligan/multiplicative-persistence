a7c9fbcbf0f497d1a5ebc30e1a2e5ff7b615778b
Tom Milligan <tommilligan@users.noreply.github.com>
2019-03-27 15:30:31 +0000

    add logging of iteration times

--- src/bin/mpersist.rs
+++ src/bin/mpersist.rs
@@ -1,5 +1,5 @@
 extern crate multiplicative_persistence;
-// use std::time::Instant;
+use std::time::Instant;
 
 use multiplicative_persistence::combinations_wr::CombinationsWithReplacement;
 use multiplicative_persistence::{multiplicative_persistence, DIGITS_HEAD, DIGITS_TAIL};
@@ -10,7 +10,7 @@ pub fn main() {
 
     let mut current_max = 2;
     for n in start_at..(start_at + num_rounds) {
-        //let round_start = Instant::now();
+        let round_start = Instant::now();
         let tail_combinations = CombinationsWithReplacement::new(DIGITS_TAIL.to_vec(), n);
         for head in DIGITS_HEAD.iter() {
             let combinations = tail_combinations.clone();
@@ -25,10 +25,10 @@ pub fn main() {
                 }
             }
         }
-        //println!(
-        //"Round {} completed: {}ms",
-        //n,
-        //round_start.elapsed().as_millis()
-        //);
+        eprintln!(
+            "Round {} completed: {}ms",
+            n,
+            round_start.elapsed().as_millis()
+        );
     }
 }
