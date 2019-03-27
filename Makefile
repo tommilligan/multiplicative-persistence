f9cb2f8994c70ed694a824cf6fe04e5fc3cd35fd
Tom Milligan <tommilligan@users.noreply.github.com>
2019-03-27 16:26:52 +0000

    added command line parsing

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: help clean coverage dev integrate lint package package-install pypi-install test upload uninstall
 
 integrate:
-	cargo run mpersist > /tmp/mpersist.stdout
+	cargo run --bin mpersist -- search > /tmp/mpersist.stdout
 	diff /tmp/mpersist.stdout integrate/mpersist.stdout
 
 test:
--- src/bin/mpersist.rs
+++ src/bin/mpersist.rs
@@ -1,15 +1,17 @@
+extern crate clap;
+
 extern crate multiplicative_persistence;
+
 use std::time::Instant;
+
+use clap::{App, Arg, SubCommand};
 
 use multiplicative_persistence::combinations_wr::CombinationsWithReplacement;
 use multiplicative_persistence::{multiplicative_persistence, DIGITS_HEAD, DIGITS_TAIL};
 
-pub fn main() {
-    let start_at = 0;
-    let num_rounds = 20;
-
+fn search(from_round: usize, num_rounds: usize) -> () {
     let mut current_max = 2;
-    for n in start_at..(start_at + num_rounds) {
+    for n in from_round..(from_round + num_rounds) {
         let round_start = Instant::now();
         let tail_combinations = CombinationsWithReplacement::new(DIGITS_TAIL.to_vec(), n);
         for head in DIGITS_HEAD.iter() {
@@ -30,5 +32,64 @@ pub fn main() {
             n,
             round_start.elapsed().as_millis()
         );
+    }
+}
+
+pub fn main() {
+    let mut app = App::new("mpersist")
+        .about("Find multiplicative persistence values")
+        .subcommand(
+            SubCommand::with_name("search")
+                .about("Search for the lowest integer with each multiplicative persistence value")
+                .arg(
+                    Arg::with_name("from_round")
+                        .help("The search round to start from (length of integer in digits)")
+                        .takes_value(true)
+                        .short("f")
+                        .long("from-round"),
+                )
+                .arg(
+                    Arg::with_name("rounds")
+                        .help("The number of search rounds to perform")
+                        .takes_value(true)
+                        .short("n")
+                        .long("rounds"),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("for")
+                .about("Get multiplicative persistence for a positive integer")
+                .arg(
+                    Arg::with_name("integer")
+                        .help("The integer to process")
+                        .index(1)
+                        .required(true),
+                ),
+        );
+    let matches = app.clone().get_matches();
+
+    if let (subcommand_name, Some(subcommand_matches)) = matches.subcommand() {
+        match subcommand_name {
+            "for" => {
+                let input_integer: &str = subcommand_matches.value_of("integer").unwrap();
+                println!("{}", multiplicative_persistence(input_integer));
+            }
+            "search" => {
+                let from_round: usize = subcommand_matches
+                    .value_of("from_round")
+                    .unwrap_or("0")
+                    .parse()
+                    .expect("Invalid integer for from_round");
+                let num_rounds: usize = subcommand_matches
+                    .value_of("rounds")
+                    .unwrap_or("15")
+                    .parse()
+                    .expect("Invalid integer from rounds");
+                search(from_round, num_rounds)
+            }
+            _ => eprintln!("Invalid subcommand."),
+        }
+    } else {
+        app.print_help().expect("Could not print help.");
     }
 }
